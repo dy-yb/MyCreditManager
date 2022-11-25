cc1c890e86b4f2e6ce65b1268603a037f550a0a3
dy-yb <daye.elin.yoon@gmail.com>
2022-11-25 11:49:10 +0900

    feat: 학생 추가

---
+++ MyCreditManager/Student.swift
@@ -0,0 +1,41 @@
+//
+//  Student.swift
+//  MyCreditManager
+//
+//  Created by Daye on 2022/11/25.
+//
+
+import Foundation
+
+struct Student {
+  let name: String
+  let gradeData: [Grades]?
+}
+
+struct Grades {
+  let subjectName: String
+  let grade: GradeValue
+  let rawGrade: Double
+}
+
+enum GradeValue: Float {
+  case AP = 4.5
+  case A = 4
+  case BP = 3.5
+  case B = 3
+  case CP = 2.5
+  case C = 2
+  case DP = 1.5
+  case D = 1
+  case F = 0
+}
+
+
+enum Menu: String {
+  case addStudent = "1"
+  case deleteStudent = "2"
+  case editOrAddGrade = "3"
+  case deleteGrade = "4"
+  case showGradeList = "5"
+  case exit = "x"
+}
--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,5 +7,49 @@
 
 import Foundation
 
-print("Hello, World!")
+var studentData: [Student] = []
 
+func menu() {
+  var isExited: Bool = false
+
+  while !isExited {
+
+    print("원하는 기능을 입력해주세요")
+    print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+
+    let selectedMenu = readLine()
+
+    if let menu = selectedMenu {
+      let selection = Menu(rawValue: menu)
+
+      switch selection {
+      case .addStudent:
+        addStudent()
+
+      case .exit:
+        isExited = true
+
+      default:
+        print("뭔가 입력이 잘못되었습니다. 1~5사이의 숫자 혹은 X를 입력해주세요.")
+
+      }
+    }
+  }
+}
+
+func addStudent() {
+  print("추가할 학생의 이름을 입력해주세요")
+
+  let newStudentName = readLine()
+
+  if let name = newStudentName {
+    studentData.append(
+      Student(
+        name: name, gradeData: nil
+      )
+    )
+  }
+}
+
+  menu()
+  print(studentData)
