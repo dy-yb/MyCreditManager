777513a0a34e99a639a29fd6a7c1d194961ba4f0
dy-yb <daye.elin.yoon@gmail.com>
2022-11-26 20:40:50 +0900

    chore: 필요없는 코드 삭제

--- MyCreditManager/Student.swift
+++ MyCreditManager/Student.swift
@@ -17,18 +17,6 @@ struct Grade {
   var grade: String
 }
 
-enum GradeValue: Float {
-  case AP = 4.5
-  case A = 4
-  case BP = 3.5
-  case B = 3
-  case CP = 2.5
-  case C = 2
-  case DP = 1.5
-  case D = 1
-  case F = 0
-}
-
 enum Menu: String {
   case addStudent = "1"
   case deleteStudent = "2"
